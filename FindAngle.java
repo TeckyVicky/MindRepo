ec25ce59fd3e870c5386e9d3efbab4d31c7ea047
Vikram <56976795+TeckyVicky@users.noreply.github.com>
2019-12-13 14:55:23 +0530

    Add files via upload

--- FindAngle.java
+++ FindAngle.java
@@ -3,13 +3,14 @@ public class FindAngle {
 
 	public static void main(String[] args) {
 		
-		findAngle(7,20);
+		findAngle(7,50);
 
 	}
 
 	private static void findAngle(int h, int m) {
 		
 		int angle = (int) (((h*30)+(m*0.5))-(m*6));
+		angle = Math.abs(angle);
 		System.out.println(angle);
 		
 	}
